93cff589129995e98d7d1fe7428b74d62ac4b43e
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2023-10-24 17:54:55 +0900

    데이터 수정 삭제 등 추가된거

---
+++ team10_24/Form2.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace team10_24
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.New = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Modify = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(252, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(263, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(252, 152);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(263, 25);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "결과 입력";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "식물 번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "식물 이름";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "식물 위치";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(161, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "식물 가격";
+            // 
+            // New
+            // 
+            this.New.Location = new System.Drawing.Point(187, 213);
+            this.New.Name = "New";
+            this.New.Size = new System.Drawing.Size(58, 23);
+            this.New.TabIndex = 9;
+            this.New.Text = "새로";
+            this.New.UseVisualStyleBackColor = true;
+            this.New.Click += new System.EventHandler(this.New_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(265, 213);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(58, 23);
+            this.Add.TabIndex = 10;
+            this.Add.Text = "추가";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Modify
+            // 
+            this.Modify.Location = new System.Drawing.Point(344, 212);
+            this.Modify.Name = "Modify";
+            this.Modify.Size = new System.Drawing.Size(58, 23);
+            this.Modify.TabIndex = 11;
+            this.Modify.Text = "수정";
+            this.Modify.UseVisualStyleBackColor = true;
+            this.Modify.Click += new System.EventHandler(this.Modify_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(424, 213);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(58, 23);
+            this.Delete.TabIndex = 12;
+            this.Delete.Text = "삭제";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(499, 212);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(58, 23);
+            this.Search.TabIndex = 13;
+            this.Search.Text = "찾기";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(252, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(263, 25);
+            this.textBox3.TabIndex = 14;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Modify);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.New);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button New;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Modify;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
--- team10_24/Program.cs
+++ team10_24/Program.cs
@@ -17,6 +17,7 @@ namespace team10_24
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
