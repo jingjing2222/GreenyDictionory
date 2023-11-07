24bad38db93e63883b3983404a2be2bc4e96697e
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2023-11-07 09:45:22 +0900

    DatabaseManger.cs, Form1.cs, Form3.cs 수정

--- team10_24/Form1.cs
+++ team10_24/Form1.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace team10_24
 {
     public partial class Form1 : Form
     {
+        private DatabaseManager dbManager;
         public Form1()
         {
             InitializeComponent();
+            dbManager = new DatabaseManager();
+            dbManager.Connect();
         }
 
         //로그인 클릭
@@ -26,14 +30,13 @@ namespace team10_24
         //회원가입 클릭
         private void Signup_Click(object sender, EventArgs e)
         {
-
+            Form3 form3 = new Form3(); // Create an instance of Form2
+            form3.Show(); // Show Form2
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
-
-        
     }
 }
--- team10_24/Form3.cs
+++ team10_24/Form3.cs
@@ -12,9 +12,12 @@ namespace team10_24
 {
     public partial class Form3 : Form
     {
+        private DatabaseManager dbManager;
         public Form3()
         {
             InitializeComponent();
+            dbManager = new DatabaseManager();
+            dbManager.Connect();
         }
 
         private void pw_check_bt_Click(object sender, EventArgs e)
@@ -24,7 +27,12 @@ namespace team10_24
 
         private void Id_check_bt_Click(object sender, EventArgs e)
         {
-
+            if (dbManager.Id_check(Id.Text))
+            {
+                MessageBox.Show("사용 가능한 아이디입니다.");
+            }
+            else
+                MessageBox.Show("이미 있는 아이디입니다.");
         }
 
         private void submit_Click(object sender, EventArgs e)
