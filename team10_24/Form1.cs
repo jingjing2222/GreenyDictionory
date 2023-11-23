5e38487b547f2f48c0ced19588507fa9f8b80217
김현우 <@DESKTOP-HGJKI2G>
2023-11-23 15:07:55 +0900

    김현우 pull

--- team10_24/Form1.cs
+++ team10_24/Form1.cs
@@ -13,12 +13,50 @@ namespace team10_24
 {
     public partial class Form1 : Form
     {
+        public class DatabaseManager
+        {
+            public int? GetUserId(string username)
+            {
+                // MySQL 데이터베이스 연결 문자열
+                string connectionString = "/* 여기에 MySQL 연결 문자열 입력 */";
+
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var query = "SELECT uid FROM UserTable WHERE id = @username";
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@username", username);
+
+                        var result = command.ExecuteScalar();
+                        if (result != null)
+                        {
+                            return Convert.ToInt32(result);
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+        public class UserData
+        {
+            public int UserId { get; set; } // 사용자의 ID
+                                            // 기존의 다른 프로퍼티들...
+        }
         private DatabaseManager dbManager;
+
         public Form1()
         {
             InitializeComponent();
             dbManager = new DatabaseManager();
             dbManager.Connect();
+        }
+        public class UserSession
+        {
+            public static UserSession Instance { get; } = new UserSession();
+
+            public int UserId { get; set; }
+            private UserSession() { }
         }
 
         //로그인 클릭
@@ -29,6 +67,7 @@ namespace team10_24
             if (userData != null && userData.Password == PW.Text)
             {
                 MessageBox.Show("로그인 성공!");
+                UserSession.Instance.UserId = userData.UserId; // 사용자의 UID 저장
                 this.Hide();
                 Form16 form16 = new Form16(); 
                 form16.ShowDialog();
--- team10_24/Form5.cs
+++ team10_24/Form5.cs
@@ -19,6 +19,33 @@ namespace team10_24
 
         private void history_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
+=======
+            // 로그인한 사용자의 UID를 12로 고정. 실제 애플리케이션에서는 로그인 세션 등을 통해 동적으로 얻어야 합니다.
+            int loggedInUserId = UserSession.Instance.UserId; // 로그인한 사용자의 UID 사용
+
+            try
+            {
+                // MySQL 데이터베이스에 연결
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    // 사용자의 UID에 해당하는 식물 이름을 조회하는 SQL 쿼리
+                    string query = @"
+                    SELECT PlantTable.plant_name 
+                    FROM PlantTable 
+                    INNER JOIN SearchHistoryTable ON PlantTable.plant_id = SearchHistoryTable.plant_id 
+                    WHERE SearchHistoryTable.uid = @UserId";
+
+                    // 쿼리 실행
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        // 쿼리에 사용자의 UID를 매개변수로 전달
+                        cmd.Parameters.AddWithValue("@UserId", loggedInUserId);
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+>>>>>>> Stashed changes
 
         }
     }
