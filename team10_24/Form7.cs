3c8d980af8438136e91cdf27f03274555b36ec40
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2023-11-30 16:23:11 +0900

    식물 일기장 보기, 추가 기능 추가

--- team10_24/Form7.cs
+++ team10_24/Form7.cs
@@ -1,35 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using team10_24;
 
 namespace team10_24
 {
     public partial class Form7 : Form
     {
-        public Form7()
+        private DatabaseManager dbManager;
+        private int userId;
+        private int diaryId;
+
+        public Form7(int userId, int diaryId)
         {
             InitializeComponent();
+            dbManager = new DatabaseManager(); // Initialize DatabaseManager
+            this.userId = userId;
+            this.diaryId = diaryId;
+            LoadDiaryEntry();
+        }
+
+        private void LoadDiaryEntry()
+        {
+            DatabaseManager.DiaryEntry entry = dbManager.GetDiaryEntry(this.userId, this.diaryId);
+            if (entry != null)
+            {
+                // Assuming you have a TextBox named diaryTextBox for displaying diary entry
+                textBox1.Text = entry.DiaryText;
+            }
         }
 
         private void Modify_Click(object sender, EventArgs e)
         {
-
+            // Logic to modify the diary entry
         }
 
         private void Delete_Click(object sender, EventArgs e)
         {
-
+            // Logic to delete the diary entry
         }
 
         private void Okay_Click(object sender, EventArgs e)
         {
-
+            // Logic for okay action, if needed
         }
 
         private void back_Click(object sender, EventArgs e)
